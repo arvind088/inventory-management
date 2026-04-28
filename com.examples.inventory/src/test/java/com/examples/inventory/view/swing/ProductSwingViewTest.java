38ef7c16e9ab2ac0a34100c9ffcef1e8435e5a09
arvind088 <avi.lmc098@gmail.com>
2026-04-28 14:43:19 +0200

    Add failing Swing view show products test

--- com.examples.inventory/src/test/java/com/examples/inventory/view/swing/ProductSwingViewTest.java
+++ com.examples.inventory/src/test/java/com/examples/inventory/view/swing/ProductSwingViewTest.java
@@ -1,18 +1,25 @@
 package com.examples.inventory.view.swing;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.edt.GuiQuery;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
 
+import com.examples.inventory.model.Product;
+
 public class ProductSwingViewTest extends AssertJSwingJUnitTestCase {
 
 	private FrameFixture window;
+	private ProductSwingView view;
 
 	@Override
 	protected void onSetUp() {
-		ProductSwingView view = GuiActionRunner.execute(new GuiQuery<ProductSwingView>() {
+		view = GuiActionRunner.execute(new GuiQuery<ProductSwingView>() {
 			@Override
 			protected ProductSwingView executeInEDT() {
 				return new ProductSwingView();
@@ -32,5 +39,18 @@ public class ProductSwingViewTest extends AssertJSwingJUnitTestCase {
 		window.button("addButton").requireText("Add");
 		window.button("updateButton").requireText("Update");
 		window.button("deleteButton").requireText("Delete");
+	}
+
+	@Test
+	public void testShowAllProducts() {
+		GuiActionRunner.execute(() ->
+			view.showAllProducts(List.of(
+				new Product("1", "Laptop", 10, 999.99),
+				new Product("2", "Mouse", 20, 19.99))));
+		String[][] contents = window.table("productTable").contents();
+		assertThat(contents).isDeepEqualTo(new String[][] {
+			{ "1", "Laptop", "10", "999.99" },
+			{ "2", "Mouse", "20", "19.99" }
+		});
 	}
 }
